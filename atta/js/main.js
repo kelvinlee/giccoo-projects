13ead4f963941e9c2486a8f4d1983e373816d705
Miller Liang <miller.lmf@gmail.com>
2015-03-04 15:57:09 +0800

    share_di
    share_differce

--- atta/js/main.js
+++ atta/js/main.js
@@ -1,3 +1,5 @@
+var _list = ["萌妹鹅","女神鹅","小清新鹅","女王鹅","极客鹅","暖男鹅","CEO鹅","大叔鹅",];
+var _list_img = ["img/share_pic0.jpg","img/share_pic1.jpg""img/share_pic2.jpg""img/share_pic3.jpg""img/share_pic4.jpg""img/share_pic5.jpg""img/share_pic6.jpg""img/share_pic7.jpg"];
 
 $(document).ready(function(){
 	$('.e').click(function(){
@@ -7,6 +9,8 @@ $(document).ready(function(){
 		$('.page'+index).addClass('animated fadeInUpBig').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function showInit(){
 				$('.page'+index).removeClass('animated fadeInUpBig');
 				addReturn();
+				document.title = _list[index];
+				$("#shareimg").attr("src",_list_img[index]);
 			});
 		});
 	
@@ -22,6 +26,8 @@ function addReturn(){
 			$('.sharePage').show();
 			$('.sharePage').click(function(){
 				$(this).hide();
+				document.title = "据说，点击这个帖子的企鹅，都坏坏地笑了！";
+				$("#shareimg").attr("src","img/share_pic.jpg");
 			});
 	});
 }
