d8da277436a8c2fe176907d5a599dd06d9175469
Miller Liang <miller.lmf@gmail.com>
2015-06-08 10:25:40 +0800

    delete flip over effect, update copy text

--- atta/ob/js/main.js
+++ atta/ob/js/main.js
@@ -1,11 +1,29 @@
 
 $(document).ready(function(){
-  $('.questionBox').click(function(){
-    console.log($(this).parent().index());
-    index = $(this).parent().index();
-    $('.a'+index).show();
-    $('.a'+index).click(function(){
-        $('.a'+index).hide();
-      });
-  });
+  $('.questionBox1').click(function(){
+                  console.log($(this).parent().index());
+                  index = $(this).parent().index();
+                  $('.a'+index).show();
+                  $('.a'+index).click(function(){
+                      $('.a'+index).hide();
+                    });
+                });
+                  // console.log(111);
+                $('.questionBox2').click(function(){
+                  console.log($(this).parent().index());
+                  index = $(this).parent().index();
+                  $('.b'+index).show();
+                  $('.b'+index).click(function(){
+                      $('.b'+index).hide();
+                    });
+                });
+                  console.log(333);
+                $('.questionBox3').click(function(){
+                  console.log($(this).parent().index());
+                  index = $(this).parent().index();
+                  $('.c'+index).show();
+                  $('.c'+index).click(function(){
+                      $('.c'+index).hide();
+                    });
+                });
 });
