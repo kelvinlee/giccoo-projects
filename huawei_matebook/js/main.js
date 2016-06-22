d983f587767e95da2eb8db6587b526be6490e947
kelvinlee <lhq945@gmail.com>
2016-06-22 12:41:33 +0800

    5

--- huawei_matebook/js/main.js
+++ huawei_matebook/js/main.js
@@ -267,7 +267,8 @@ $(document).ready(function load (){
 		},2500)
 
 		setTimeout(function(){
-			window.location.href="http://redbox.wepiao.com/index.html?pid=acf13e85ef280289&channelid=3&chid=100";
+			//window.location.href="http://redbox.wepiao.com/index.html?pid=acf13e85ef280289&channelid=3&chid=100";
+			window.location.href="http://www.baidu.com";
 		},3500)
 	}
 
