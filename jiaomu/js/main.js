29b5f034603b28074ecc42bd186a74449e5d34d6
liamH <levf@qq.com>
2016-08-24 01:54:35 +0800

    3
    3

--- jiaomu/js/main.js
+++ jiaomu/js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function load (){
 	$("#btn").click(function(){
 		window.location.href="http://www.lancome.com.cn/landing/genifique.html";
 	})
+
+	var iflike=0
+
+	$("#like").click(function(){
+		$("#like2").css({"display":"block"});
+		if (iflike==0) {
+			//这里反馈
+		};
+
+		iflike=1
+
+	})
+
 });
