3334829eb003dd9035ce5ea671e67018ffaee229
Miller Liang <miller.lmf@gmail.com>
2016-12-21 12:11:57 +0800

    fix bugs

--- ariel_chefnic/js/main_1221.js
+++ ariel_chefnic/js/main_1221.js
@@ -1,8 +1,8 @@
 /**
  * Created by miller on 16/9/7.
  */
-$(document).ready(function () {
-    
+$(document).ready(function() {
+
     var videoNum = 12;
     var dishNum = 12;
     var _ipApiUrl = 'http://api.giccoo.com/api/ip/';
@@ -15,7 +15,7 @@ $(document).ready(function () {
     //禁止滚动
     var enabledTouch = true;
     // $(document).on("touchstart touchmove mousedown mousemove", function (event) {
-    $(document).on("touchmove mousemove", function (event) {
+    $(document).on("touchmove mousemove", function(event) {
         var tag = $(event.target).parents()[0].tagName;
         var tagclass = $(event.target).parents(".share").size();
         var thistag = event.target.tagName;
@@ -31,7 +31,8 @@ $(document).ready(function () {
         _currentDevice = 'MOBILE';
         window.onResize = _onResize();
         _onResize();
-        function _onResize(){
+
+        function _onResize() {
             var docEl = document.documentElement;
             var default_value = 640;
             var clientWidth = docEl.clientWidth;
@@ -45,78 +46,79 @@ $(document).ready(function () {
         /*var _pcVideo = '<embed src="http://player.video.qiyi.com/a4d5301564a7d7f98ecce09ce8284523/0/0/w_19rsw0ni5x.swf-albumId=6263790809-tvId=6263790809-isPurchase=0-cnId=20" allowFullScreen="true" quality="high" width="100%" height="100%" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>';
         $('#mobile_video').remove();
         $('.main_video').append(_pcVideo);*/
-        
+
         var docEl = document.documentElement;
         docEl.style.fontSize = 100 + 'px';
     };
 
     /*获取视频/!**/
-    $('.video_back').css('display','none');
+    $('.video_back').css('display', 'none');
     // $('#video_click').css('display','none');
-    var _hideVideos=[];
-    $.getJSON(_getVideoPicsUrl,function (data) {
-       if (data.reason === 'success') {
-           var _data = data.info;
-           console.log(_data);
-           _data.reverse();
-           console.log(_data);
-           for (var i = 0; i < _data.length; i++) {
-               var obj = _data[i];
-               var _pic = _picBeforeUrl + obj.image;
-               var _link = obj.link;
-               var _name =obj.name;
-               var _id = 'video' + i;
-               var _dom = '<div class="small_video" id=' + _id + '><a target="_blank" href=' + _link + '><img class="video_pic" src=' + _pic + '></a><div class="videoName">' +_name +'</div></div>';
-               $('.pic_container').append(_dom);
+    var _hideVideos = [];
+    $.getJSON(_getVideoPicsUrl, function(data) {
+        if (data.reason === 'success') {
+            var _data = data.info;
+            console.log(_data);
+            _data.reverse();
+            console.log(_data);
+            for (var i = 0; i < _data.length; i++) {
+                var obj = _data[i];
+                var _pic = _picBeforeUrl + obj.image;
+                var _link = obj.link;
+                var _name = obj.name;
+                var _id = 'video' + i;
+                var _dom = '<div class="small_video" id=' + _id + '><a target="_blank" href=' + _link + '><img class="video_pic" src=' + _pic + '></a><div class="videoName">' + _name + '</div></div>';
+                $('.pic_container').append(_dom);
 
-               if (i != 0) {
-                   _hideVideos.push(_id)
-               }
-           }
-           console.log(_hideVideos);
-           function controlVideoDom(show) {
-               if(show == true){
-                   for (var i = 0; i < _hideVideos.length; i++) {
-                       var obj = _hideVideos[i];
-                       $('#'+obj).css('display','block');
-                   }
-                   $('.video_back').css('display','block');
-                   $('#video_click').css('display','none');
-               }else {
-                   for (var k = 0; k < _hideVideos.length; k++) {
-                       var objK = _hideVideos[k];
-                       $('#'+objK).css('display','none');
-                   }
-                   $('.video_back').css('display','none');
-                   $('#video_click').css('display','block');
-               }
-           }
-           //视频控制
-           $('.video_back').css('display','none');
-           if(_hideVideos.length != 1 && _hideVideos.length != 0){
-               controlVideoDom(false);
-               $('.video_back').css('display','none');
-               $('#video_click').on('click',function () {
-                   event.preventDefault();
-                   controlVideoDom(true);
-               });
-               $('.video_back').on('click',function () {
-                   event.preventDefault();
-                   controlVideoDom(false);
-               });
-           }else {
-               //如果只有一个视频,点击按钮消失
-               $('#video_click').css('display','none');
-           }
+                if (i != 0) {
+                    _hideVideos.push(_id)
+                }
+            }
+            console.log(_hideVideos);
 
-       }else {
-           console.log(data.reason);
-       }
+            function controlVideoDom(show) {
+                if (show == true) {
+                    for (var i = 0; i < _hideVideos.length; i++) {
+                        var obj = _hideVideos[i];
+                        $('#' + obj).css('display', 'block');
+                    }
+                    $('.video_back').css('display', 'block');
+                    $('#video_click').css('display', 'none');
+                } else {
+                    for (var k = 0; k < _hideVideos.length; k++) {
+                        var objK = _hideVideos[k];
+                        $('#' + objK).css('display', 'none');
+                    }
+                    $('.video_back').css('display', 'none');
+                    $('#video_click').css('display', 'block');
+                }
+            }
+            //视频控制
+            $('.video_back').css('display', 'none');
+            if (_hideVideos.length != 1 && _hideVideos.length != 0) {
+                controlVideoDom(false);
+                $('.video_back').css('display', 'none');
+                $('#video_click').on('click', function() {
+                    event.preventDefault();
+                    controlVideoDom(true);
+                });
+                $('.video_back').on('click', function() {
+                    event.preventDefault();
+                    controlVideoDom(false);
+                });
+            } else {
+                //如果只有一个视频,点击按钮消失
+                $('#video_click').css('display', 'none');
+            }
+
+        } else {
+            console.log(data.reason);
+        }
     });
 
     //获取菜谱
     var dishArray = [];
-    $.getJSON(_getDishUrl,function (data) {
+    $.getJSON(_getDishUrl, function(data) {
         if (data.reason === 'success') {
             var _data = data.info;
             _data.reverse();
@@ -126,7 +128,7 @@ $(document).ready(function () {
                 var _link = obj.link;
                 var _avatar = _picBeforeUrl + obj.avatar;
                 var name = obj.name;
-                var username =  obj.username;
+                var username = obj.username;
                 // var _tips = _picBeforeUrl + obj.tips;
                 // var _id = obj.id;
                 var dish = {
@@ -134,33 +136,33 @@ $(document).ready(function () {
                     'id': obj.id
                 };
                 dishArray.push(dish);
-                 // ' <a  target="_blank" href=' + _link + '><div class="small_video" id=' + _id + '><img src=' + _pic + '></div></a>'
-                var _dom = '<div class="dish"><div class="dishLeft"><a class="dishLink" target="_blank" href='+ _link + '><img class="dish_pic" src=' + _image + '></a></div><div class="dish_info"><img class="dish_makerPic" src='+ _avatar +' alt=""> <div class="user_name">'+ username + '</div> <div class="dish_name">' + name +'</div> <div class="tips_fakeBtn">碧浪TIPS</div> </div><div class="tips_btn"></div></div>';
+                // ' <a  target="_blank" href=' + _link + '><div class="small_video" id=' + _id + '><img src=' + _pic + '></div></a>'
+                var _dom = '<div class="dish"><div class="dishLeft"><a class="dishLink" target="_blank" href=' + _link + '><img class="dish_pic" src=' + _image + '></a></div><div class="dish_info"><img class="dish_makerPic" src=' + _avatar + ' alt=""> <div class="user_name">' + username + '</div> <div class="dish_name">' + name + '</div> <div class="tips_fakeBtn">碧浪TIPS</div> </div><div class="tips_btn"></div></div>';
                 $('#dishContanier').append(_dom);
             }
             console.log(dishArray);
-            $('.tips_btn').on('click',function (e) {
+            $('.tips_btn').on('click', function(e) {
                 enabledTouch = false;
                 var _num = $(this).parent().index();
                 console.log(_num);
-                $('.tips_path').attr('src',dishArray[_num].tips);
-                $('.tips_overlay').css('display','block');
+                $('.tips_path').attr('src', dishArray[_num].tips);
+                $('.tips_overlay').css('display', 'block');
 
             });
             // console.log(_dishPic);
-        }else {
+        } else {
             console.log(data.reason);
         }
     });
 
-    $('.lottery_btn').on('click',function () {
+    $('.lottery_btn').on('click', function() {
         enabledTouch = false;
-        $('.lottery_overlay').css('display','block');
-        $('.start').css('display','block');
+        $('.lottery_overlay').css('display', 'block');
+        $('.start').css('display', 'block');
     });
 
 
-    $('.colseBtn').on('click',function () {
+    $('.colseBtn').on('click', function() {
         enabledTouch = true;
         $('.tips_overlay').fadeOut(400);
     });
@@ -180,18 +182,20 @@ $(document).ready(function () {
         var reg = /^(0|86|17951)?(13|15|17|18|14)[\d]{9}$/;
         return reg.test(value);
     }
-    $('.post_number').on("click", function () {
+    $('.post_number').on("click", function() {
         var number = $(".number").find('input').val(); //手机
-        if(!checkMobile(number)){
+        if (!checkMobile(number)) {
             alert("请输入正确手机号!");
-        }else {
-            $('.start').css('display','none');
-            $('.loading').css('display','block');
+        } else {
+            $('.start').css('display', 'none');
+            $('.loading').css('display', 'block');
 
 
             //POST 抽奖
-            var _data = {'mobile': number};
-            $.post(_lotteryApiUrl,_data,function (data, status) {
+            var _data = {
+                'mobile': number
+            };
+            $.post(_lotteryApiUrl, _data, function(data, status) {
                 console.log(data);
                 nextResult();
                 /*if(data.recode == 201){
@@ -203,25 +207,26 @@ $(document).ready(function () {
                     $(".number").find('input').val("");
                 }*/
             });
+
             function nextResult() {
-                $.getJSON(_ipApiUrl,function(data){
+                $.getJSON(_ipApiUrl, function(data) {
                     // alert("数据: " + data + "\n状态: " + status);
-                    if(data.reason == 'success'){
+                    if (data.reason == 'success') {
                         var _linkUrl = "";
                         var address = data.info.content.address;
                         console.log(address);
-                        if(address == "北京市"){
+                        if (address == "北京市") {
                             $('.result_copy').text("不用气馁,前往官网了解更多有奖活动！");
-                            $('.link_btn').text("前往官网");
+                            // $('.link_btn').text("前往官网");
                             _linkUrl = "http://www.pg.com.cn/products/detail.aspx?id=30";
 
-                        }else{
+                        } else {
                             $('.result_copy').text("现在购买碧浪商品，有更多大奖等你来拿！");
-                            $('.link_btn').text("前往商城");
+                            // $('.link_btn').text("前往商城");
 
-                            if(_currentDevice === "PC"){
+                            if (_currentDevice === "PC") {
                                 _linkUrl = "http://sale.jd.com/act/gB3aNFEkQIr4qVx.html";
-                            }else{
+                            } else {
                                 _linkUrl = "http://sale.jd.com/m/act/gB3aNFEkQIr4qVx.html";
                             }
 
@@ -230,20 +235,20 @@ $(document).ready(function () {
                         $('.result_copy').text("现在购买碧浪商品，不仅有满199减100，还有价值2000元的海尔洗衣机等你来抢！");
                         // $('.link_btn').text("前往商城");
 
-                        if(_currentDevice === "PC"){
+                        if (_currentDevice === "PC") {
                             _linkUrl = "http://sale.jd.com/act/gB3aNFEkQIr4qVx.html";
-                        }else{
+                        } else {
                             _linkUrl = "http://sale.jd.com/m/act/gB3aNFEkQIr4qVx.html";
                         }
 
-                        $('.link_btn').on('click',function () {
-                            window.open(_linkUrl,'_blank')
+                        $('.link_btn').on('click', function() {
+                            window.open(_linkUrl, '_blank')
                         });
-                        $('.loading').fadeOut(800,function () {
+                        $('.loading').fadeOut(800, function() {
                             $('.result').fadeIn();
                         });
 
-                    }else {
+                    } else {
                         alert("请求失败")
                     }
 
