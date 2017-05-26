3d2f9703901a8878236172fc5aa16de01efe2cc0
liamH <levf@qq.com>
2017-05-26 16:35:54 +0800

    maxshow
    share

--- maxshow/js/main.js
+++ maxshow/js/main.js
@@ -1,4 +1,34 @@
 
 $(document).ready(function load (){
-	
+	loadWechatConfig();
+  	wx.ready(function() {
+    var shareContent;
+    shareContent = {
+      title: "智·混动 MAX秀",
+      desc: "",
+      link: "http://m.giccoo.com/maxshow/",
+      imgUrl: "http://m.giccoo.com/maxshow/img/ico.jpg",
+      success: function() {},
+      cancel: function() {}
+    };
+    wx.onMenuShareTimeline(shareContent);
+    wx.onMenuShareAppMessage(shareContent);
+    wx.onMenuShareQQ(shareContent);
+    return wx.onMenuShareWeibo(shareContent);
+  });
+
+
+
+
+
 });
+
+
+var loadWechatConfig = function() {
+  var hm, s, url;
+  url = encodeURIComponent(window.location.href.split("#")[0]);
+  hm = document.createElement('script');
+  hm.src = "http://api.giccoo.com/api/config?url=" + url;
+  s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+};
