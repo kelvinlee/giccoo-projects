fe75aa767cd55baaac69c02c4df3bcf661e722d7
kelvinlee <lhq945@gmail.com>
2017-08-11 00:41:10 +0800

    Create elr4

---
+++ elr4/js/main.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function load (){
+
+	// checkNum()
+
+	var nowNum=0
+
+
+	function checkNum(){
+		$.get("http://api.giccoo.com/count/get/jiaomu1",function(msg){
+				if (msg.recode == 200) {
+					//console.log("update success", msg.info)
+					//alert(msg.info[0].count)
+					nowNum=msg.info[0].count
+					setText()
+				}else{
+					//alert("get faild")
+				}
+		})
+	}
+
+	function setText(){
+		$("#likeNum").text(parseInt(nowNum)+"人喜欢");
+	}
+
+	$("#btn").click(function(){
+		window.location.href="http://r4.esteelauderclub.cn/mob/index.html";
+	})
+
+	var iflike=0
+
+	$("#like").click(function(){
+		$("#like2").css({"display":"block"});
+		if (iflike==0) {
+			//这里反馈
+
+			//=======
+			$.post("http://api.giccoo.com/count/update", {id: "jiaomu1"},function(msg){
+				if (msg.recode == 200) {
+					//alert("update success")
+					nowNum++
+					setText()
+				}else{
+					//alert("update faild")
+				}
+			})
+			//=======
+		};
+
+		iflike=1
+
+	})
+
+});
