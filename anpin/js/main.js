32d997cbc520b4dfb4903b1c34f2b0aa6ac628cc
levf <levf@qq.com>
2017-08-29 16:07:13 +0800

    counter
    counter

--- anpin/js/main.js
+++ anpin/js/main.js
@@ -1,17 +1,45 @@
 
 $(document).ready(function load (){
+	loadWechatConfig();
+  	wx.ready(function() {
+    var shareContent;
+    shareContent = {
+      title: "您有一件「生鲜」护肤品抵达，请查收",
+      desc: "",
+      link: "http://m.giccoo.com/anpin/",
+      imgUrl: "http://m.giccoo.com/anpin/img/ico.jpg",
+      success: function() {},
+      cancel: function() {}
+    };
+    wx.onMenuShareTimeline(shareContent);
+    wx.onMenuShareAppMessage(shareContent);
+    wx.onMenuShareQQ(shareContent);
+    return wx.onMenuShareWeibo(shareContent);
+  });
 
+  	var mydate = new Date();
 	checkNum()
 
-	var nowNum=0
+	var nowNum=0-295
 
+ 	var ri=mydate.getDate()
+	var yue=mydate.getMonth()
+    var num80=(yue-5)*30+(ri-7)*82
 
 	function checkNum(){
+		
+
+		
+		
+		
+
+
+
 		$.get("http://api.giccoo.com/count/get/jiaomu1",function(msg){
 				if (msg.recode == 200) {
 					//console.log("update success", msg.info)
 					//alert(msg.info[0].count)
-					nowNum=msg.info[0].count
+					nowNum=msg.info[0].count+num80
 					setText()
 				}else{
 					//alert("get faild")
@@ -52,3 +80,17 @@ $(document).ready(function load (){
 	})
 
 });
+
+
+
+
+
+
+var loadWechatConfig = function() {
+  var hm, s, url;
+  url = encodeURIComponent(window.location.href.split("#")[0]);
+  hm = document.createElement('script');
+  hm.src = "http://api.giccoo.com/api/config?url=" + url;
+  s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+};
