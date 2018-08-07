5bd9e2600b97c1d68013d56e56fa432e7a4e0402
levf <levf@qq.com>
2018-08-07 13:23:34 +0800

    wow
    test

--- wow/js/ani.js
+++ wow/js/ani.js
@@ -22,7 +22,7 @@ function setFireDot(){
     dotC.position.set(Math.random()*640,Math.random()*stageH+stageH/2*.7)
     dot.pivot.set(25,25)
     dotC.scale.x=dotC.scale.y=(Math.random()*Math.random()*.8+.2)*2
-    dotC.blendMode=_ADD
+    //================dotC.blendMode=_ADD
     var _t=Math.random()*2+1
     //var _delay=Math.random()*2+1
     dot.alpha=0
