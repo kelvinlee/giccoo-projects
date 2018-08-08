cfea688aa0c5c7655d18c21f65bfd1c92c0ad536
levf <levf@qq.com>
2018-08-08 22:52:19 +0800

    wow
    test

--- wow/js/ani.js
+++ wow/js/ani.js
@@ -13,23 +13,25 @@ function setPage1(){
 
   setMask()
   //setDisplaceMent()
-  setScorll()
+  
   
   stage1.addChild(fireDots)
   setFireDot()
 
   setTrail()
+
+  setScorll()
 }
-function setDisplaceMent(){
-  stage1.addChild(p1bg)
-  stage1.addChild(p1mask1)
-  var disF=new PIXI.filters.DisplacementFilter(p1mask1)
-  stage1.filters=[disF]
-  disF.scale.x=15
-  disF.scale.y=0
-  p1mask1.height=stageH*2
-  TweenMax.to(p1mask1,2,{y:-stageH/8*2,repeat:1000,ease:Linear.easeNone})
-}
+// function setDisplaceMent(){
+//   stage1.addChild(p1bg)
+//   stage1.addChild(p1mask1)
+//   var disF=new PIXI.filters.DisplacementFilter(p1mask1)
+//   stage1.filters=[disF]
+//   disF.scale.x=15
+//   disF.scale.y=0
+//   p1mask1.height=stageH*2
+//   TweenMax.to(p1mask1,2,{y:-stageH/8*2,repeat:1000,ease:Linear.easeNone})
+// }
 
 function setMask(){
   stage1.addChild(p1bg2)
@@ -57,7 +59,7 @@ function setFireDot(){
     dotC.position.set(Math.random()*640,Math.random()*stageH+stageH/2*.7)
     dot.pivot.set(25,25)
     dotC.scale.x=dotC.scale.y=(Math.random()*Math.random()*.8+.2)*2
-    dotC.blendMode=_ADD//================
+    //dotC.blendMode=_ADD//================
     var _t=Math.random()
     //var _delay=Math.random()*2+1
     dot.alpha=0
@@ -107,12 +109,76 @@ function setTrail(){
 
   blurF.blur=16
   //trailSprite.filters=[blurF]
-   //showLayer.blendMode=_ADD
+   showLayer.blendMode=_ADD
    //trailSprite.blendMode=_ADD
 
 }
 
-
+var papper=pSprite("img/papper_s.png")
+var scorll1=pSprite("img/score.png")
+var scorll2=pSprite("img/score.png")
+var stamp1=pSprite("img/stamp1.png")
+var stamp2=pSprite("img/stamp2.png")
+var p2bg=pSprite("img/p2bg.jpg")
+var p2copy=pSprite("img/copy.png")
+var scorll=new PIXI.Container()
+var papperMask=new PIXI.Graphics()
 function setScorll(){
+
+  app.stage.addChild(scorll)
+  scorll.addChild(papper)
+  scorll.addChild(p2copy)
+  scorll.addChild(scorll1)
+  scorll.addChild(scorll2)
+  scorll.addChild(stamp2)
+  scorll.addChild(stamp1)
+  papper.width=640
+  papper.height=632
+  scorll1.pivot.set(320,42)
+  scorll1.position.set(320,42)
+  scorll2.pivot.set(320,42)
+  scorll2.position.set(320,42)
+  scorll2.y=632-42
+  scorll2.rotation=Math.PI
+
+  papper.pivot.set(240,237)
+  papper.position.set(320,stageH/2)
+  scorll1.y=stageH/2-32//stageH/2-316+42
+  scorll2.y=stageH/2+32//stageH/2+316-42
+  p2copy.y=stageH/2
+  p2copy.pivot.y=316
+  stamp1.pivot.y=stamp2.pivot.y=53
+  stamp1.pivot.x=stamp2.pivot.x=320
+  stamp1.y=stamp2.y=stageH/2
+  stamp1.x=stamp2.x=320
+  TweenMax.to(stamp1,1,{alpha:0,yoyo:true,repeat:1000})
+
+  scorll.addChild(papperMask)
+  p2copy.alpha=0
+  papper.mask=papperMask
+  papperMask.beginFill(0xffffff,0)
+  papperMask.drawRect(0,0,640,64)
+  papperMask.pivot.y=32
+  papperMask.y=stageH/2
+
+  //TweenMax.to(papperMask,1,{height:632,yoyo:true,repeat:1000})
+  TweenMax.to(scorll,2,{y:-stageH*.03,yoyo:true,repeat:1000,ease:Sine.easeInOut})
+
+  scorll.interactive=true
+  scorll.touchstart=openScorll
+}
+
+function openScorll(){
+  console.log("open")
+  TweenMax.to(scorll,2,{y:0})
+  TweenMax.to(stamp1,.2,{alpha:0})
+  TweenMax.to(stamp2,.2,{alpha:0})
+
+  TweenMax.to(stamp1.scale,.2,{x:1.5,y:1.5})
+  TweenMax.to(stamp2.scale,.2,{x:1.5,y:1.5})
+
+  TweenMax.to(papperMask,1.5,{height:632})
+  TweenMax.to(scorll1,1.5,{y:stageH/2-316+42})
+  TweenMax.to(scorll2,1.5,{y:stageH/2+316-42})
 
 }
