3bc74a944ec797157107bb9f3f2ee724fd8a3f00
levf <levf@qq.com>
2018-08-17 00:12:43 +0800

    308
    done

--- df-308/js/ani.js
+++ df-308/js/ani.js
@@ -1,397 +1,183 @@
-var question1A=["Q：你内心所孕育的情感世界，","Q：你内心所培育的社交圈，","Q：你内心所向往的职场，"]
-var question2A=["是一片怎样的光景？","是一片多大的乐园？","会停留在列车的哪个站台?"]
-var answer1A=["一个人的浩瀚星空>>","秘友间的小小花园>> ","追梦者的奋进都市>>"]
-var answer2A=["两个人的温柔宇宙>>","交际花的玫瑰花海>> ","造梦者的轻活家园>>"]
-var audioPlaying = false
-
-var aniQA//=[aniQ1C,aniQ2C,aniQ3C]
-
-function showQ (_Qnum) {
-	question1.x=56
-  question1.y=stageH/2-387
-  question1.text=question1A[_Qnum]
-  question2.x=110
-  question2.y=stageH/2-329
-  question2.text=question2A[_Qnum]
-  answer1.x=169
-  answer1.y=stageH/2-186
-  answer1.text=answer1A[_Qnum]
-  answer2.x=169
-  answer2.y=stageH/2-26
-  answer2.text=answer2A[_Qnum]
-  question1.alpha=question2.alpha=answer1.alpha=answer2.alpha=btn1.alpha=btn2.alpha=1
-
-
-  btn1.position.set(320,stageH/2-173)
-  btn2.position.set(320,stageH/2-18)
-  var _t=.05
-  TweenMax.from(question1,1,{y:"-=150",alpha:0,delay:_t*3,ease:Back.easeOut})
-  TweenMax.from(question2,1,{y:"-=150",alpha:0,delay:_t*2,ease:Back.easeOut})
-  TweenMax.from(answer1  ,1,{y:"-=150",alpha:0,delay:_t*1,ease:Back.easeOut})
-  TweenMax.from(answer2  ,1,{y:"-=150",alpha:0,delay:_t*0,ease:Back.easeOut})
-  TweenMax.from(btn1	   ,1,{y:"-=120",alpha:0,delay:_t*1,ease:Back.easeOut})
-  TweenMax.from(btn2	   ,1,{y:"-=120",alpha:0,delay:_t*0,ease:Back.easeOut})
-  if(_Qnum==0){aniQ1()}
-  if(_Qnum==1){aniQ2()}
-  if(_Qnum==2){aniQ3()}
-  btn1.interactive=true
- 	btn2.interactive=true
+var please
+var btnNext
+var btnNextMusic
+function setMusicPage(){
+	setDisc()
+	setSelector()
+	showSelector()
+	please=new Sprite(getTe(_CDN+"img/please.png"))
 	
+	please.y=stageH/2+5
+	//_CDN+"img/btn_nextmusic.png",
+	btnNext=new Sprite(getTe(_CDN+"img/btn_next.png"))
+	btnNextMusic=new Sprite(getTe(_CDN+"img/btn_nextmusic.png"))
+
+	btnNext.y=btnNextMusic.y=stageH/2+384*stageH/1138
+
+	pStage.addChild(please,btnNext,btnNextMusic)
+	btnNext.interactive=true
+	btnNextMusic.interactive=true
+	btnNext.touchstart=goNext
+	btnNextMusic.touchstart=goNext
+}
+//=======================黑胶碟
+var disc1,disc2,disc3,disc4,discC,rotatDisc
+function setDisc(){
+	disc1fadeOut=new Sprite(getTe(_CDN+"img/disc1.png"))
+	disc1=new Sprite(getTe(_CDN+"img/disc1.png"))
+	disc2=new Sprite(getTe(_CDN+"img/disc2.png"))
+	disc2b=new Sprite(getTe(_CDN+"img/disc2.png"))
+	disc3=new Sprite(getTe(_CDN+"img/disc3.png"))
+	disc4=new Sprite(getTe(_CDN+"img/disc4.png"))
+	discC=new Container()
+	rotatDisc=new Container()
+	pStage.addChild(discC)
+	rotatDisc.addChild(disc1,disc2,disc2b,disc3,)
+	discC.addChild(rotatDisc,disc1fadeOut,disc4)
+	disc1fadeOut.pivot.x=disc1.pivot.x=disc2.pivot.x=disc2b.pivot.x=disc3.pivot.x=200.5
+	disc1fadeOut.pivot.y=disc1.pivot.y=disc2.pivot.y=disc2b.pivot.y=disc3.pivot.y=200.5
+	disc1fadeOut.x=disc1.x=disc2.x=disc2b.x=disc3.x=320
+	disc1fadeOut.y=disc1.y=disc2.y=disc2b.y=disc3.y=stageH/2-260*stageH/1138
+
+	disc4.pivot.set(41,19)
+	disc4.position.set(112,stageH/2-408*stageH/1138)
+	disc1fadeOut.alpha=0
+	discPlay()
+
 }
 
-function hideQ(){
-	btn1.interactive=false
- 	btn2.interactive=false
-	var _Qnum=Qnum
-	question1.x=56
-  question1.y=stageH/2-387
-  question1.text=question1A[_Qnum]
-  question2.x=110
-  question2.y=stageH/2-329
-  question2.text=question2A[_Qnum]
-  answer1.x=169
-  answer1.y=stageH/2-186
-  answer1.text=answer1A[_Qnum]
-  answer2.x=169
-  answer2.y=stageH/2-26
-  answer2.text=answer2A[_Qnum]
-
-  btn1.position.set(320,stageH/2-173)
-  btn2.position.set(320,stageH/2-18)
-  var _t=.05
-  TweenMax.to(question1,.5,{y:"-=150",alpha:0,delay:_t*0})
-  TweenMax.to(question2,.5,{y:"-=150",alpha:0,delay:_t*1})
-  TweenMax.to(answer1  ,.5,{y:"-=150",alpha:0,delay:_t*2})
-  TweenMax.to(answer2  ,.5,{y:"-=150",alpha:0,delay:_t*3})
-  TweenMax.to(btn1	   ,.5,{y:"-=150",alpha:0,delay:_t*2})
-  TweenMax.to(btn2	   ,.5,{y:"-=150",alpha:0,delay:_t*3})
-  aniQA=[aniQ1C,aniQ2C,aniQ3C]
-  TweenMax.to(aniQA[_Qnum],.5,{y:"-=200",alpha:0,delay:_t*4,onComplete:showAnswer})
-
+function discPlay(){
+	TweenMax.to(disc4,.5,{rotation:0})
+	TweenMax.to(disc3,3.5,{rotation:Math.PI*2,repeat:1000,ease:Linear.easeNone})
+	disc2.alpha=.2
+	TweenMax.to(disc2,3.5,{alpha:.2,rotation:Math.PI*2,repeat:1000,ease:Linear.easeNone})
+	TweenMax.to(disc2b.scale,1,{x:.9,y:.9,repeat:1000,yoyo:true,ease:Linear.easeNone})
 }
 
-function NextQuestion(){
-	console.log("11")
-	Qnum++
-	if(Qnum<3){
-		showQ(Qnum)
-	}else{
-		console.log("三题答完")
-	}
-}
-var resultA=[9,9,9]
-var answerT1,answerT2,answerT3,answerT4,answerBtnT,answerNextT,tStyle2,tStyle3,tStyle4,soundBtn,answerA//,answer1C,answer2C,answer3C
-function setAnswer(){
-	answerT1A=[["经常一个人独处","不在乎朋友的多少","凡事都想做的更好"],["果汁分你一半","聚会从不缺席","与世无争的佛系"]]
-	answerT2A=[["享受自由的孤独","却会把知音当作宝","竞争也从来不愿失败"],["恩爱从不间断","朋友圈里永远有你","浸润出你对万物的宽逸"]]
-	answerT3A=[["就连听的音乐","时常也会哼起一首歌","耳机里的音乐"],["就连听的音乐","时常也会哼起一首歌","耳机里的音乐"]]
-	answerT4A=[["也释放着独身主义的温度","诉说你和秘友间的小打小闹","循环出你永不止步的宏愿"],["也透露着对你侬我侬的期盼","与万千朋友分享生活心语","伴随你在夏夜听见的蝉鸣"]]
-
-
-	tStyle2=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 31})
-	tStyle3=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 41})
-	tStyle4=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 32,fill:["#6a4c00"]})
-	tStyle5=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 28,fill:["#6a4c00"]})
-	answerT1=new PIXI.Text("经常一个人独处",tStyle2)
-	answerT2=new PIXI.Text("经常一个人独处",tStyle2)
-	answerT3=new PIXI.Text("经常一个人独处",tStyle2)
-	answerT4=new PIXI.Text("也释放着独身主义的温度",tStyle3)
-	answerBtnT=new PIXI.Text("音乐为我发声",tStyle4)
-	answerNextT=new PIXI.Text("下一题>>",tStyle5)
-
-	answerT1.x=answerT2.x=answerT3.x=answerT4.x=86
-
-	answerT1.y=stageH/2-400
-	answerT2.y=stageH/2-345
-	answerT3.y=stageH/2-291
-	answerT4.y=stageH/2-238
-
-	answerBtnT.position.set(123,stageH/2-152)
-	answerNextT.position.set(442,stageH/2-152)
-
-	soundBtn=new Sprite(getTe(_CDN+"img/btn_up.png"))
-	soundBtn.pivot.set(320,58)
-	soundBtn.position.set(238,stageH/2-138)
-	soundBtn.scale.x=soundBtn.scale.y=.66
-	// soundBtn, answerBtnT
-	pStage.addChild(answerT1,answerT2,answerT3,answerT4,answerNextT)
-	setSoundIcon()
-	answerA=[answerT1,answerT2,answerT3,answerT4,answerBtnT,answerNextT,soundBtn,soundIcon,answerPicC]
-	for (var i = 0; i < answerA.length; i++) {
-		answerA[i].visible=false
-	};
+function discStop(){
+	TweenMax.to(disc4,.5,{rotation:-Math.PI*2/8})
+	TweenMax.to(disc3,3.5,{rotation:0})
+	disc2.alpha=.2
+	TweenMax.to(disc2,1,{alpha:0})
+	TweenMax.to(disc2b.scale,1,{x:.9,y:.9})
 }
 
-function showAnswer(){
-	console.log("showAnswer")
-	resultA[Qnum]=nowAnswer
-	answerT1.y=stageH/2-400
-	answerT2.y=stageH/2-345
-	answerT3.y=stageH/2-291
-	answerT4.y=stageH/2-238
-
-	answerBtnT.position.set(123,stageH/2-152)
-	answerNextT.position.set(442,stageH/2-152)
-	soundBtn.position.set(238,stageH/2-138)
-	soundIcon.position.set(324,stageH/2-152)
-	answerPicC.y=0
-	var picA=[[a1a,a1b],[a2a,a2b],[a3a,a3b]]
-	// picA[Qnum][nowAnswer].alpha=1
-	TweenMax.set(picA[Qnum][nowAnswer],{alpha:1})
-	if (Qnum != 2) {
-		TweenMax.from(picA[Qnum][nowAnswer],.7,{alpha:0,x:"-=100",delay:1})
-	}else{
-		TweenMax.from(picA[Qnum][nowAnswer],.7,{alpha:0,delay:1})
-	}
-	for (var i = 0; i < answerA.length; i++) {
-		answerA[i].visible=true
-		TweenMax.set(answerA[i],{alpha:1})
-		TweenMax.from(answerA[i],.5,{alpha:0,y:"+=100",delay:.05*i})
-	};
-	answerT1.text=answerT1A[nowAnswer][Qnum]
-	answerT2.text=answerT2A[nowAnswer][Qnum]
-	answerT3.text=answerT3A[nowAnswer][Qnum]
-	answerT4.text=answerT4A[nowAnswer][Qnum]
-
-	if(Qnum==2){
-		tStyle4=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 28,fill:["#6a4c00"]})
-		answerBtnT.style=tStyle4
-		answerBtnT.text="听听你内心的声音"
-		answerBtnT.x-=15
-		soundIcon.x+=12
-		answerNextT.text="全部提交>>"
-	}
-
-	answerNextT.interactive=true
-	answerNextT.tap=hideAnswer//showQ(Qnum)
-
-	soundBtn.interactive=true
-	soundBtn.tap=playSound
-	//NextQuestion()
-	playSound()
-}
-var musicA=[["a-1","a-2"],["b-2","b-1"],["c-1","c-2"]]
-function playSound(){
-	if (_public.playing) {
-		playAudio(musicA[Qnum][nowAnswer])	
-	}
-	return false
-	if (audioPlaying) {
-		audioPlaying = false
-		PIXI.sound.pause(musicA[Qnum][nowAnswer])
-		soundIconStop()
-		return false
-	}
-	PIXI.sound.play(musicA[Qnum][nowAnswer],{complete: function(){
-		soundIconStop()
-		hideAnswer()
-		audioPlaying = false
-	},loaded: function(){
-		console.log("loaded")
-	}})
-	soundIconPlay()
-	audioPlaying = true
-}
-function hideAnswer(){
-	stopAllAudio()
-	soundIconStop()
-	PIXI.sound.stopAll()
-	audioPlaying = false
-	soundBtn.interactive=false
-	answerNextT.interactive=false
-
-	if(Qnum==2){
-		// finishAll()
-		main.pageHand()
-	}else{
-		for (var i = 0; i < answerA.length; i++) {
-			TweenMax.to(answerA[i],.5,{alpha:0,y:"+=100",delay:.05*(answerA.length-i)})
+//===================选项
+var selectorA=[[],[],[],[]]
+var selectorC
+var selectorY=[0,66,132,198]
+var selectorPivotY=[14.5,22.5,21,24.5]
+var btnBG
+function setSelector(){
+	var i,j
+	selectorC=new Container()
+	btnBG=new Sprite(getTe(_CDN+"img/btnbg.png"))
+	btnBG.pivot.set(320,34)
+	btnBG.position.set(320,0)
+	btnBG.visible=false
+	pStage.addChild(selectorC)
+	selectorC.addChild(btnBG)
+	for (i = 1; i <=4 ; i++) {
+		for (j = 1; j <= 4; j++) {
+			var _btn=new Sprite(getTe(_CDN+"img/a"+i+j+".png"))
+			selectorA[i-1][j-1]=_btn
+			selectorC.addChild(_btn)
+			_btn.position.set(320,stageH/2+(120+selectorY[j-1])*stageH/1138)
+			_btn.pivot.set(320,selectorPivotY[j-1])
+			_btn.visible=false
+			_btn.interactive=true
+			_btn.tap=selectOne
 		};
-		TweenMax.set(this,{onComplete:NextQuestion,delay:0.6})
+	};
+}
+var nowMusic=0
+var nowSelected=999
+var userAnswer=[0,0,0,0]
+function showSelector(){
+	for (var i = 0; i < 4; i++) {
+		selectorA[nowMusic][i].visible=true
+		TweenMax.from(selectorA[nowMusic][i],1.5,{y:"+=50",ease:Elastic.easeOut,delay:.05*i,overwrite:0})
+		TweenMax.from(selectorA[nowMusic][i],1.5,{alpha:0,delay:.05*i})
+	};
+}
+
+
+function selectOne(_e){
+	console.log(_e.target)
+	var i,j
+	for (i = 1; i <=4 ; i++) {
+		for (j = 1; j <= 4; j++) {
+			if(_e.target==selectorA[i-1][j-1]){
+				console.log(j-1)
+				nowSelected=j-1
+				btnBG.visible=true
+				btnBG.y=_e.target.y
+				TweenMax.set(btnBG,{rotation:0})
+				TweenMax.from(btnBG,1.5,{rotation:Math.PI*2/16,overwrite:0,ease:Elastic.easeOut})
+				TweenMax.from(btnBG.scale,1.2,{x:0.5,y:2,ease:Elastic.easeOut})
+				TweenMax.to(_e.target.scale,.2,{x:1.2,y:1.2})
+				//userAnswer[nowMusic]=j-1
+				//userAnswer[j-1]+=1
+			}else{
+				TweenMax.to(selectorA[i-1][j-1].scale,.2,{x:1,y:1})
+			}
+		};
+	};
+}
+
+//=========下一个
+function goNext(){
+	
+	if(nowSelected==999){
+		TweenMax.set(please,{x:0})
+		TweenMax.from(please,1,{x:"+=50",ease:Elastic.easeOut})
+	}else if(nowMusic<3){
+		console.log("下首歌")
+		if(nowMusic==2){btnNextMusic.visible=false}
+		for (var i = 0; i < 4; i++) {
+			selectorA[nowMusic][i].visible=false
+		};
+		userAnswer[nowSelected]++
+		nowMusic++
+		nowSelected=999
+		btnBG.visible=false
+		showSelector()
+
+	}else{
+		console.log("去结束页")
+		userAnswer[nowSelected]++
+		console.log(userAnswer)
+		showResult()
 	}
-	
-	//NextQuestion()
-	
-}
-var tStyle5,qr,logo
-function finishAll(){
-	
-	answerNextT.visible=soundBtn.visible=answerBtnT.visible=soundIcon.visible=false
-	tStyle5=new PIXI.TextStyle({fontFamily:'yrd-Medium',fontSize: 31,fill:["#000000"]})
-	answerT1.style=answerT2.style=answerT3.style=answerT4.style=tStyle5
-	answerT1.text="人生的每一帧"
-	answerT2.text="都来自内在心声的力量"
-	answerT3.text="兰蔻小黑瓶从肌底开始"
-	answerT4.text="让你由内而外闪耀出自信光芒"
-	answerT1.y=stageH/2-334
-	answerT2.y=stageH/2-284
-	answerT3.y=stageH/2-234
-	answerT4.y=stageH/2-184
-
-	logo=new Sprite(getTe(_CDN+"img/homepage-logo.png"))
-	qr=new Sprite(getTe(_CDN+"img/qr.png"))
-	pStage.addChild(qr,logo)
-	qr.y=stageH/2-124
-	logo.x=-80
-	logo.y=stageH/2-423
-	logo.scale.x=logo.scale.y=.68
-
-	main.sharePost()
-}
-var btnlottery
-function setLottery() {
-	// 抽奖 btn
-	qr.visible = false
-	btnlottery=new Sprite(getTe(_CDN+"img/btnlottery.png"))
-	pStage.addChild(btnlottery)
-	btnlottery.y=stageH/2-131
-	btnlottery.interactive=true
-	btnlottery.tap=main.openPop
-	// click to main.openPop()
 }
 
-//==========================================喇叭按钮动画
-var soundIcon,sound0,sound1,sound2,sound3
-function setSoundIcon(){
-	soundIcon=new Container()
-	sound0=new Sprite(getTe(_CDN+"img/sound0.png"))
-	sound1=new Sprite(getTe(_CDN+"img/sound1.png"))
-	sound2=new Sprite(getTe(_CDN+"img/sound2.png"))
-	sound3=new Sprite(getTe(_CDN+"img/sound3.png"))
-	// pStage.addChild(soundIcon)
-	soundIcon.addChild(sound0,sound1,sound2,sound3)
-	soundIcon.position.set(324,stageH/2-152)
-	//===
-	soundIconPlay()
-	soundIconStop()
-}
-function soundIconPlay(){
-	TweenMax.to(sound1,.5,{alpha:0,repeat:1000,yoyo:true,delay:0})
-	TweenMax.to(sound2,.5,{alpha:0,repeat:1000,yoyo:true,delay:.15})
-	TweenMax.to(sound3,.5,{alpha:0,repeat:1000,yoyo:true,delay:.3})
-}
-function soundIconStop(){
-	TweenMax.killTweensOf(sound1)
-	TweenMax.killTweensOf(sound2)
-	TweenMax.killTweensOf(sound3)
-	TweenMax.set(sound1,{alpha:1})
-	TweenMax.set(sound2,{alpha:1})
-	TweenMax.set(sound3,{alpha:1})
-}
-//=======================================答案下部出现动画
-var answerPicC,floor,table,a1a,a1b,a2a,a2b,a3a,a3b
-function setAnswerPic(){
-	answerPicC=new Container()
-	floor=new Graphics()
-	table=new Sprite(getTe(_CDN+"img/a1pic1.png"))
-
-	a1a=new Sprite(getTe(_CDN+"img/a1a.png"))
-	a1b=new Sprite(getTe(_CDN+"img/a1b.png"))
-	a2a=new Sprite(getTe(_CDN+"img/a2a.png"))
-	a2b=new Sprite(getTe(_CDN+"img/a2b.png"))
-	a3a=new Sprite(getTe(_CDN+"img/a3a.jpg"))
-	a3b=new Sprite(getTe(_CDN+"img/a3b.jpg"))
-
-	pStage.addChild(answerPicC)
-	answerPicC.addChild(floor,a3a,a3b,table,a1a,a1b,a2a,a2b)
-	floor.beginFill(0xaeb6c0,1)
-  floor.drawRect(0,stageH/2+182,640,stageH)
-  table.position.set(100,stageH/2+220-150)
-
-  a1a.alpha=a1b.alpha=a2a.alpha=a2b.alpha=a3a.alpha=a3b.alpha=0
-
-  a1a.y=a1b.y=stageH/2-32
-  a1b.x=-100
-  a2a.y=a2b.y=stageH/2+157
-
-	a3a.x=a3b.x=640-218
-  a3a.y=a3b.y=stageH/2-427
-  answerPicC.alpha=0
-  answerPicC.y=100
-}
-
-
-//=======================================问题下部出现动画
-var aniQ1C,aniQ2C,aniQ3C,q1pic1,q1pic2,q1pic3,q1pic4
-function aniQ1(){
-	console.log("-----------------------")
-
-	aniQ1C=new Container()
-	aniQ2C=new Container()
-	aniQ3C=new Container()
-	pStage.addChild(aniQ1C)
-	q1pic1=new Sprite(getTe(_CDN+"img/q1pic1.png"))
-	q1pic2=new Sprite(getTe(_CDN+"img/q1pic2.png"))
-	q1pic3=new Sprite(getTe(_CDN+"img/q1pic3.png"))
-	q1pic4=new Sprite(getTe(_CDN+"img/q1pic4.png"))
-	aniQ1C.addChild(q1pic2,q1pic3,q1pic1,q1pic4)
-	var q1picA=[q1pic1,q1pic2,q1pic3,q1pic4]
-	for (var i = 0; i < q1picA.length; i++) {
-		q1picA[i].pivot.set(320,148)
-		q1picA[i].position.set(320,stageH/2+280)
-	};
-	q1picA[3].pivot.set(376,81)
-	q1picA[3].position.set(376,stageH/2+280-148+81)
-	TweenMax.from(q1pic1.scale,.8,{x:0,y:0,ease:Back.easeOut})
-	TweenMax.from(q1pic2,.8,{x:"-=320",ease:Back.easeOut,delay:.2})
-	TweenMax.from(q1pic3,.8,{x:"+=320",ease:Back.easeOut,delay:.1})
-	TweenMax.from(q1pic4,1.2,{y:"-=80",ease:Bounce.easeOut,delay:.5})
-	TweenMax.from(q1pic4.scale,1.2,{x:0,y:0,ease:Elastic.easeOut,delay:0})
-
-}
-var q2pic1,_q2pic1,q2pic2,q2pic3,q2pic4,q2pic5,q2pic6,q2pic7
-var q2picA
-function aniQ2(){
-	pStage.addChild(aniQ2C)
-	q2picA=[q2pic1,q2pic2,q2pic3,q2pic4,q2pic5,q2pic6,q2pic7]
-	var i
-	for (i = 2; i <= 7; i++) {
-		q2picA[i-1]=new Sprite(getTe(_CDN+"img/q2pic"+i+".png"))
-		aniQ2C.addChild(q2picA[i-1])
-		q2picA[i-1].pivot.set(320,190)
-		q2picA[i-1].position.set(320,stageH/2+306)
-	};
-
-	q2pic1=new Sprite(getTe(_CDN+"img/q2pic1.png"))
-	aniQ2C.addChild(q2pic1)
-	q2pic1.pivot.set(45.5,97)
-	q2pic1.position.set(222,stageH/2+399)
-
-	_q2pic1=new Sprite(getTe(_CDN+"img/q2pic1.png"))
-	aniQ2C.addChild(_q2pic1)
-	_q2pic1.pivot.set(45.5,97)
-	_q2pic1.position.set(427,stageH/2+399)
-
-	q2picA[0]=q2pic1
-	q2picA.unshift(_q2pic1)
-	console.log(5,q2picA[5])
-	for (i = 0; i < q2picA.length; i++) {
-		if(i<3){
-			TweenMax.from(q2picA[i],.8,{y:"-=150",ease:Back.easeNone,delay:i*.1,alpha:0})
-		}else{
-			TweenMax.from(q2picA[i],1.5,{y:"-=150",ease:Bounce.easeOut,delay:i*.05+.2,alpha:0})
+//=========结束
+var result
+function showResult(){
+	btnNext.visible=false
+	for (var i = 0; i < 4; i++) {
+		if(userAnswer[i]>=userAnswer[0]&&userAnswer[i]>=userAnswer[1]&&userAnswer[i]>=userAnswer[2]&&userAnswer[i]>=userAnswer[3]){
+			result=i
+			console.log(i)
 		}
-		
 	};
+	if(userAnswer[0]*userAnswer[1]*userAnswer[2]*userAnswer[3]==1){
+		result=parseInt(Math.random()*4)
+		console.log("1111===",result)
+	}
 
-}
-var q3pic1,_q3pic2
-function aniQ3(){
-
-	q3pic1=new Sprite(getTe(_CDN+"img/q3pic1.png"))
-	q3pic2=new Sprite(getTe(_CDN+"img/q3pic2.png"))
-
-	pStage.addChild(aniQ3C)
-	aniQ3C.addChild(q3pic2,q3pic1)
-
-	q3pic1.pivot.set(320,203)
-	q3pic2.pivot.set(320,203)
-	q3pic1.position.set(320,stageH/2+285)
-	q3pic2.position.set(320,stageH/2+285)
-	TweenMax.from(q3pic1,2.5,{x:"+=70",ease:Elastic.easeOut})
-	TweenMax.from(q3pic2,2.5,{x:"-=70",ease:Elastic.easeOut,delay:.1})
-
-	TweenMax.from(q3pic1,.5,{alpha:0})
-	TweenMax.from(q3pic2,.5,{alpha:0,delay:.1})
+	bgA[result].visible=true
+	r11.visible=true
+	r2A[result].visible=true
+	r3A[result].visible=true
+	r4A[result].visible=true
+	endbtn1.visible=true
+	endbtn2.visible=true
+	var AAA=[r11,r4,r3,r2,endbtn1,endbtn2]
+	for (var j = 0; j < 4; j++) {
+		
+		TweenMax.from(AAA[j],2.5,{y:"+=150",ease:Elastic.easeOut,delay:.05*j,overwrite:0})
+		TweenMax.from(AAA[j],1.5,{alpha:0,delay:.05*j,overwrite:0})
+	};
 
 }
--- df-308/js/build.js
+++ df-308/js/build.js
@@ -1,13 +1,57 @@
 var _CDN = "./"
 var imageList = [
-	_CDN+"img/test.png",
+	_CDN+"img/bg.jpg",
+	_CDN+"img/disc1.png",
+	_CDN+"img/disc2.png",
+	_CDN+"img/disc3.png",
+	_CDN+"img/disc4.png",
+	_CDN+"img/a11.png",
+	_CDN+"img/a12.png",
+	_CDN+"img/a13.png",
+	_CDN+"img/a14.png",
+	_CDN+"img/a21.png",
+	_CDN+"img/a22.png",
+	_CDN+"img/a23.png",
+	_CDN+"img/a24.png",
+	_CDN+"img/a31.png",
+	_CDN+"img/a32.png",
+	_CDN+"img/a33.png",
+	_CDN+"img/a34.png",
+	_CDN+"img/a41.png",
+	_CDN+"img/a42.png",
+	_CDN+"img/a43.png",
+	_CDN+"img/a44.png",
+	_CDN+"img/btnbg.png",
+	_CDN+"img/please.png",
+	_CDN+"img/btn_next.png",
+	_CDN+"img/btn_nextmusic.png",
+	_CDN+"img/bg1.jpg",
+	_CDN+"img/bg2.jpg",
+	_CDN+"img/bg3.jpg",
+	_CDN+"img/bg4.jpg",
+	_CDN+"img/r11.png",
+	_CDN+"img/r12.png",
+	_CDN+"img/r13.png",
+	_CDN+"img/r14.png",
+	_CDN+"img/r22.png",
+	_CDN+"img/r23.png",
+	_CDN+"img/r24.png",
+	_CDN+"img/r32.png",
+	_CDN+"img/r33.png",
+	_CDN+"img/r34.png",
+	_CDN+"img/r42.png",
+	_CDN+"img/r43.png",
+	_CDN+"img/r44.png",
+	_CDN+"img/qr.png",
+	_CDN+"img/endbtn1.png",
+	_CDN+"img/endbtn2.png",
 ];
 var _NORMAL=PIXI.BLEND_MODES.NORMAL,
     _ADD=PIXI.BLEND_MODES.ADD,
     _MULTIPLY=PIXI.BLEND_MODES.MULTIPLY,
     _SCREEN=PIXI.BLEND_MODES.SCREEN;
 
-var pStage,bg,question1,question2,answer1,answer2,tStyle,btn1,btn2,Qnum=0
+var pStage,bg
 var stageH
 var buildUGC = function () {
 	console.log("app",this,this.opts.w,this.opts.h)
@@ -24,55 +68,104 @@ var buildUGC = function () {
 }
 
 function setup(){
-	
+	bg=new Sprite(getTe(_CDN+"img/bg.jpg"))
+	pStage.addChild(bg)
+	bg.width=640
+	bg.height=stageH
+	setMusicPage()
+	setResultPage()
 }
-var nowAnswer
-function btn1Down(){
-	btn1.texture=getTe(_CDN+"img/btn_down.png")
-	answer1.x+=10
-	answer1.y-=10
-	pStage.interactive=true
-	pStage.touchend=allBtnUp
-	nowAnswer=0
-	console.log(musicA[Qnum][nowAnswer])
-	audio = document.getElementById(musicA[Qnum][nowAnswer])
-	audio.load()
+var bg1,bg2,bg3,bg4,bgC,bgA
+var r11
+var r2,r3,r4
+var r2A,r3A,r4A
+var r12,r13,r14,r22,r23,r24,r32,r33,r34,r42,r43,r44
+var qr
+var endbtn1,endbtn2
+function setResultPage(){
+	bgC=new Container()
+	bg1=new Sprite(getTe(_CDN+"img/bg1.jpg"))
+	bg2=new Sprite(getTe(_CDN+"img/bg2.jpg"))
+	bg3=new Sprite(getTe(_CDN+"img/bg3.jpg"))
+	bg4=new Sprite(getTe(_CDN+"img/bg4.jpg"))
+	bgA=[bg1,bg2,bg3,bg4]
+	bgC.addChild(bg1,bg2,bg3,bg4)
+	pStage.addChild(bgC)
+	bgC.height=stageH+1
+	bg1.visible=bg2.visible=bg3.visible=bg4.visible=false
+	r11=new Sprite(getTe(_CDN+"img/r11.png"))
+	r11.pivot.y=569
+	r11.y=stageH/2
+	r11.visible=false
+	r2=new Container()
+	r3=new Container()
+	r4=new Container()
+
+	var r12=new Sprite(getTe(_CDN+"img/r12.png"))
+	var r22=new Sprite(getTe(_CDN+"img/r22.png"))
+	var r32=new Sprite(getTe(_CDN+"img/r32.png"))
+	var r42=new Sprite(getTe(_CDN+"img/r42.png"))
+	r2.addChild(r12,r22,r32,r42)
+	r2A=[r12,r22,r32,r42]
+
+	var r13=new Sprite(getTe(_CDN+"img/r13.png"))
+	var r23=new Sprite(getTe(_CDN+"img/r23.png"))
+	var r33=new Sprite(getTe(_CDN+"img/r33.png"))
+	var r43=new Sprite(getTe(_CDN+"img/r43.png"))
+	r3.addChild(r13,r23,r33,r43)
+	r3A=[r13,r23,r33,r43]
+
+	var r14=new Sprite(getTe(_CDN+"img/r14.png"))
+	var r24=new Sprite(getTe(_CDN+"img/r24.png"))
+	var r34=new Sprite(getTe(_CDN+"img/r34.png"))
+	var r44=new Sprite(getTe(_CDN+"img/r44.png"))
+	r4.addChild(r14,r24,r34,r44)
+	r4A=[r14,r24,r34,r44]
+
+
+	r12.pivot.y=r22.pivot.y=r32.pivot.y=r42.pivot.y=r13.pivot.y=r23.pivot.y=r33.pivot.y=r43.pivot.y=r14.pivot.y=r24.pivot.y=r34.pivot.y=r44.pivot.y=569
+	r12.y=r22.y=r32.y=r42.y=r13.y=r23.y=r33.y=r43.y=r14.y=r24.y=r34.y=r44.y=stageH/2
+
+	for (var i = 0; i <4 ; i++) {
+		r2A[i].visible=false
+		r3A[i].visible=false
+		r4A[i].visible=false
+	};
+
+	pStage.addChild(bgC,r11,r2,r3,r4)
+
+
+//=======
+	qr=new Sprite(getTe(_CDN+"img/qr.png"))
+	qr.pivot.y=569+20
+	qr.y=stageH/2
+	qr.visible=false
+
+	endbtn1=new Sprite(getTe(_CDN+"img/endbtn1.png"))
+	endbtn2=new Sprite(getTe(_CDN+"img/endbtn2.png"))
+	endbtn1.position.set(84,stageH/2+372)
+	endbtn2.position.set(339,stageH/2+372)
+
+	endbtn1.visible=endbtn2.visible=false
+
+	endbtn1.interactive=true
+	endbtn2.interactive=true
+
+	endbtn1.touchstart=goShare
+	endbtn2.touchstart=goMore
+
+	pStage.addChild(qr,endbtn1,endbtn2)
 }
 
-function btn1Up(){
-	btn1.texture=getTe(_CDN+"img/btn_up.png")
-	answer1.x-=10
-	answer1.y+=10
-	console.log("A")
-	nowAnswer=0
-	hideQ()
-}
-function btn2Down(){
-	btn2.texture=getTe(_CDN+"img/btn_down.png")
-	answer2.x+=10
-	answer2.y-=10
-	pStage.interactive=true
-	pStage.touchend=allBtnUp
-	nowAnswer=1
-	console.log(musicA[Qnum][nowAnswer])
-	audio = document.getElementById(musicA[Qnum][nowAnswer])
-	audio.load()
+
+function goShare(){
+	endbtn1.visible=endbtn2.visible=false
+	qr.visible=true
+	console.log("分享海报")
 }
 
-function btn2Up(){
-	btn2texture=getTe(_CDN+"img/btn_up.png")
-	answer2.x-=10
-	answer2.y+=10
-	console.log("B")
-	nowAnswer=1
-	hideQ()
+function goMore(){
+	console.log("了解更多")
 }
 
-function allBtnUp(){
-	btn1.texture=getTe(_CDN+"img/btn_up.png")
-	btn2.texture=getTe(_CDN+"img/btn_up.png")
-	answer1.x=169
-	answer1.y=stageH/2-186
-	answer2.x=169
-  answer2.y=stageH/2-26
-}
+
