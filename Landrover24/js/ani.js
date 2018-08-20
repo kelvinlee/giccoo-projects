d80ec2fad3203f88d4239a9a65ea72826e379176
levf <levf@qq.com>
2018-08-20 20:01:56 +0800

    L24
    list

--- Landrover24/js/ani.js
+++ Landrover24/js/ani.js
@@ -331,7 +331,7 @@ function goForm(){
 }
 function goList(){
 	console.log("去歌单")
-	window.location.href='https://music.163.com/#/playlist?id=2369232527';
+	window.location.href='https://music.163.com/#/playlist?id=2369232527&userid=1554475702';
 }
 //==========================================P3
 var p3top,p3btn1,page3,p3target,p3playing
