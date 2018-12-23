fd5c7c811d4a67231e63c6301d8890c4b81c848f
liam <levf@qq.com>
2018-12-24 02:58:53 +0800

    cdn

--- emoji/js/build.js
+++ emoji/js/build.js
@@ -1,5 +1,5 @@
 //var _CDN = "./";
-var _CDN = "//image.giccoo.com/projects/emoji/";
+var _CDN = "//image.giccoo.com/projects/emoji1/";
 var imageList = [
 	_CDN+"img/p1bg.jpg",
 	_CDN+"img/bg_top.png",
